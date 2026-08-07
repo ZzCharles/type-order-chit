9fb8869a07bc8ef3267b49259ae22699cc2d3887
ZzCharles <charlesbobby253@gmail.com>
2026-08-07 17:09:04 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* The Pass service worker
    Bump CACHE when you change index.html, or phones will keep serving the old copy. */
-const CACHE = 'the-pass-v5';
+const CACHE = 'the-pass-v6';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png'];
 
 self.addEventListener('install', e => {
